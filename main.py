6fbe1dcb3ddbcc5f5f9840f0f940065e55788591
SuperShrew <joshua.gerrish@outlook.com>
2025-05-27 18:25:01 +0100

    started adding master loop

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 from cell import *
 from microbe import *
 from colours import *
+import time
+import os
+
+environment = [[Cell("blank", -1)]*20]*20
+
+# testing stuff below \/
+print(environment)
 
 test_struct = [
 	[Cell("producer", 1), Cell("blank", 1), Cell("producer", 1)],
 	[Cell("blank", 1), Cell("mouth", 1), Cell("blank", 1)], 
 	[Cell("producer", 1), Cell("blank", 1), Cell("producer", 1)]]
 
-test = Microbe(3, test_struct, 100, [4, 4])
+test = Microbe(3, test_struct, 100, [4, 4], 1)
+
 
 for i in test.structure:
 	for x in i:
 		print(x.icon, end=" ")
 	print()
+
+
+
 print()
 print(test.size)
 print(test.max_age)
 print(test.health)
+
+# end testing stuff
+
+# assigning variables
+
+microbes = [test]
+
+while True:
+	for i in microbes:
+		i.tick()
+	time.sleep(0.2)
+	for i in range(0, len(environment)): # iterate numerically through first layer of environment
+		for x in range(0, len(environment[0])): # iterate numerically through each item in the layer of environment
+			if environment[i][x].type == "food":
+				pass
+			else: 
+				environment[i][x] = Cell("blank", -1)
--- microbe.py
+++ microbe.py
@@ -3,11 +3,12 @@ import random
 
 
 class Microbe:
-	def __init__(self, size, structure, life_mult, coords):
+	def __init__(self, size, structure, life_mult, coords, id):
 		self.size = size # defines the hitbox of the cell
 		self.structure = structure # 2D array of cells
 		self.coords = coords # top left corner of hitbox
 		self.cell_amount = 0
+		self.id = id
 
 		for i in self.structure:
 			for x in i:
@@ -16,9 +17,13 @@ class Microbe:
 		
 		self.max_age = self.cell_amount*life_mult
 		self.health = self.cell_amount
+		self.food = 0
 
 	def tick(self):
 		"""
 		returns a dictionary of cells to update
 		"""
 		pass # still trying to figure out how i would update cells ourside the structure
+
+	def draw(self, env):
+		pass
