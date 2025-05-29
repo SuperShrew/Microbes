6f3bc6848d9f54d016bd52b8c00cc01487dca2c9
SuperShrew <joshua.gerrish@outlook.com>
2025-05-29 15:52:55 +0100

    added food cell

--- cell.py
+++ cell.py
@@ -9,6 +9,7 @@ class Cell:
             "producer": self.c.GREEN + "P",
             "killer": self.c.RED + "K",
             "armour": self.c.MAGENTA + "A",
+            "food": self.c.BLUE + "F",
             "blank": " "
         }
         self.type = type
