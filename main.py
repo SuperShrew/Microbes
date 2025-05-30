6ea22ac7b8314afeb6529d99cefa731a4042603e
SuperShrew <joshua.gerrish@outlook.com>
2025-05-30 12:25:13 +0100

    organism working

--- main.py
+++ main.py
@@ -4,31 +4,46 @@ from colours import *
 import time
 import os
 
-environment = [[Cell("blank", -1)]*20]*20
+environment = [[Cell("blank", -1) for _ in range(0, 20)] for _ in range(0, 20)]
 
 # testing stuff below \/
 print(environment)
 
-test_struct = [
-	[Cell("producer", 1), Cell("blank", 1), Cell("producer", 1)],
-	[Cell("blank", 1), Cell("mouth", 1), Cell("blank", 1)], 
-	[Cell("producer", 1), Cell("blank", 1), Cell("producer", 1)]]
+#test_struct = [
+#	[Cell("producer", 1), Cell("blank", 1), Cell("producer", 1)],
+#	[Cell("blank", 1), Cell("mouth", 1), Cell("blank", 1)], 
+#	[Cell("producer", 1), Cell("blank", 1), Cell("producer", 1)]]
 
-test = Microbe(3, test_struct, 100, [4, 4], 1)
+test_struct = {
+	(0, 0): Cell("producer", 1),
+	(0, 2): Cell("producer", 1),
+	(1, 1): Cell("mouth", 1),
+	(2, 0): Cell("producer", 1),
+	(2, 2): Cell("producer", 1)
+}
+
+test = Microbe(test_struct, 100, [4, 4], 1)
 
 
-for i in test.structure:
-	for x in i:
-		print(x.icon, end=" ")
-	print()
-
+#for i in test.structure:
+#	for x in i:
+#		print(x.icon, end=" ")
+#	print()
 
 
 print()
 print(test.size)
 print(test.max_age)
 print(test.health)
+for i in test.structure:
+	environment[test.coords[1]+i[1]][test.coords[0]+i[0]] = test.structure[i]
 
+def draw_env():
+	global environment
+	for i in range(0, len(environment)):
+		for x in environment[i]:
+			print(x.icon, end=" ")
+		print()
 # end testing stuff
 
 # assigning variables
@@ -37,7 +52,10 @@ microbes = [test]
 
 while True:
 	for i in microbes:
-		i.tick()
+		environment = i.tick(microbes, environment)
+		print("food:", i.food)
+		print("health:", i.health)
+	draw_env()
 	time.sleep(0.2)
 	for i in range(0, len(environment)): # iterate numerically through first layer of environment
 		for x in range(0, len(environment[0])): # iterate numerically through each item in the layer of environment
@@ -45,3 +63,6 @@ while True:
 				pass
 			else: 
 				environment[i][x] = Cell("blank", -1)
+	os.system("clear")
+	for i in microbes:
+		i.draw(environment)
--- microbe.py
+++ microbe.py
@@ -3,27 +3,79 @@ import random
 
 
 class Microbe:
-	def __init__(self, size, structure, life_mult, coords, id):
-		self.size = size # defines the hitbox of the cell
-		self.structure = structure # 2D array of cells
+	def __init__(self, structure, life_mult, coords, id, size=0):
+		self.size = size # defines the hitbox of the  (not currently needed)
+		self.structure = structure # dictionary of cells and their coords relative to the top left corner
 		self.coords = coords # top left corner of hitbox
-		self.cell_amount = 0
+		self.cell_amount = len(structure)
 		self.id = id
-
-		for i in self.structure:
-			for x in i:
-				if not(x.type == "blank"):
-					self.cell_amount += 1
-		
 		self.max_age = self.cell_amount*life_mult
 		self.health = self.cell_amount
 		self.food = 0
+		self.food_counter = 50
+		self.mover = False
 
-	def tick(self):
-		"""
-		returns a dictionary of cells to update
-		"""
-		pass # still trying to figure out how i would update cells ourside the structure
+		for i in self.structure:
+			if structure[i].type == "mover":
+				self.mover = True
+				break
+
+	def tick(self, microbes, env):
+		if self.food_counter > 0:
+			self.food_counter -= 1
+		elif self.food_counter == 0 and self.food > 0:
+			self.food -= 1
+			self.food_counter = 50
+		elif self.food_counter == 0 and self.food == 0:
+			self.health -= 0.1
+
+		if self.health <= 0:
+			for i in range(0, len(microbes)):
+				if microbes[i].id == self.id:
+					microbes.pop(i)
+
+		if self.mover: # IMPORTANT: ADD COLLISIONS ASAP
+			direction = random.randint(1, 2)
+			if direction == 1:
+				self.coords[0] += random.randint(-1, 1)
+			if direction == 2:
+				self.coords[1] += random.randint(-1, 1)
+		else:
+			for i in self.structure:
+				if self.structure[i].type == "producer":
+					if random.randint(1, 100) >= 75:
+						side = random.randint(1, 4)
+						if side == 1:
+							if env[self.coords[1] + i[1]+1][self.coords[0] + i[0]].type == "blank":
+								env[self.coords[1] + i[1]+1][self.coords[0] + i[0]] = Cell("food", -1)
+						elif side == 2:
+							if env[self.coords[1] + i[1]][self.coords[0] + i[0]+1].type == "blank":
+								env[self.coords[1] + i[1]][self.coords[0] + i[0]+1] = Cell("food", -1)
+						elif side == 3:
+							if env[self.coords[1] + i[1]-1][self.coords[0] + i[0]].type == "blank":
+								env[self.coords[1] + i[1]-1][self.coords[0] + i[0]] = Cell("food", -1)
+						elif side == 4:
+							if env[self.coords[1] + i[1]][self.coords[0] + i[0]-1].type == "blank":
+								env[self.coords[1] + i[1]][self.coords[0] + i[0]-1] = Cell("food", -1)
+		for i in self.structure:
+			if self.structure[i].type == "mouth":
+				if env[self.coords[1] + i[1]+1][self.coords[0] + i[0]].type == "food":
+					env[self.coords[1] + i[1]+1][self.coords[0] + i[0]] = Cell("blank", -1)
+					self.food += 1
+				if env[self.coords[1] + i[1]][self.coords[0] + i[0]+1].type == "food":
+					env[self.coords[1] + i[1]][self.coords[0] + i[0]+1] = Cell("blank", -1)
+					self.food += 1
+				if env[self.coords[1] + i[1]-1][self.coords[0] + i[0]].type == "food":
+					env[self.coords[1] + i[1]-1][self.coords[0] + i[0]] = Cell("blank", -1)
+					self.food += 1
+				if env[self.coords[1] + i[1]][self.coords[0] + i[0]-1].type == "food":
+					env[self.coords[1] + i[1]][self.coords[0] + i[0]-1] = Cell("blank", -1)
+					self.food += 1
+
+		# TODO: program killer
+		return env
 
 	def draw(self, env):
-		pass
+		for i in self.structure:
+			env[self.coords[1]+i[1]][self.coords[0]+i[0]] = self.structure[i]
+		return env
